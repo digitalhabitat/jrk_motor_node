10f81ece2cba6c92b04498c17ec773bc2d6a915a
Michael Miller <miketm89@gmail.com>
2019-05-15 12:26:49 -0400

    fixed stuff

--- scripts/jrk_subscribe.py
+++ scripts/jrk_subscribe.py
@@ -3,16 +3,17 @@
 import rospy
 import std_msgs
 from ackermann_msgs.msg import AckermannDrive
+import swri_rospy
 
 import serial
 import time
-ser = serial.Serial( "/dev/jrk", 9600)
+ser = serial.Serial( "/dev/ttyACM0", 9600)
 #ser = serial.Serial( "/dev/ttyACM0", 115200)
 print("connected to: " + ser.portstr)
 
 def handle_jrk_targets(AckermannDrive):#function name*
-	target=int(AckermannDrive.steering_angle)+100
-	print 'target: %s' % target
+	target=int(AckermannDrive.steering_angle)*2353+2014
+	print('target: %s' % target)
 	lowByte = (target & ord("\x1F")) | ord("\xC0")
 	highByte = (target >> 5) & ord("\x7F")
 	print("about to write", lowByte, highByte)
@@ -20,5 +21,6 @@ def handle_jrk_targets(AckermannDrive):#function name*
 	ser.write(chr(highByte))    
 
 rospy.init_node('jrk_steering_node', anonymous=True)
-rospy.Subscriber('ackermann', AckermannDrive, handle_jrk_targets)#subscribing to joy teleop
+##rospy.Subscriber('ackermann', AckermannDrive, handle_jrk_targets)#subscribing to joy teleop
+swri_rospy.Subscriber('ackermann', AckermannDrive, handle_jrk_targets , queue_size=10)
 rospy.spin()
