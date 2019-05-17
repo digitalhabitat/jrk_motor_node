19a9310760955d7920809a9cfc1629f53260e4b4
mikewq <miketm89@gmail.com>
2019-05-16 23:09:15 -0400

    Fixed code for postive steering angle to correspond with left turn

--- scripts/jrk_subscribe_orig.py
+++ scripts/jrk_subscribe_orig.py
@@ -7,12 +7,13 @@ from ackermann_msgs.msg import AckermannDrive
 import serial
 import time
 
-# need to call jrk2cmd.. persistent name udev rules not wokring
+# TODO this node could be made better, 
+# need to call jrk2cmd.. persistent name udev rules not working
 import os 
 
 cmd_port = os.popen('jrk2cmd --cmd-port').read()
 print(cmd_port.rstrip("\n"))
-rospy.loginfo("jrk cmd_port = %s", cmd_port)
+rospy.loginfo("jrk cmd_port = %s ignore parameter dev", cmd_port)
 
 # Parameters
 DEV = rospy.get_param("~dev", "/dev/ttyACM0")
@@ -25,15 +26,17 @@ ser = serial.Serial( cmd_port.rstrip("\n") , BAUD)
 print("connected to: " + ser.portstr)
 rospy.loginfo("connected: %s", ser.portstr)
 
-def handle_jrk_targets(AckermannDrive):#function name*
-	target=-1*int(AckermannDrive.steering_angle*2500+2048)
-        rospy.loginfo("target = %d",target)
-	print 'target: %s' % target
-	lowByte = (target & ord("\x1F")) | ord("\xC0")
-	highByte = (target >> 5) & ord("\x7F")
-	print("about to write", lowByte, highByte)
-	ser.write(chr(lowByte))
-	ser.write(chr(highByte))    
+def handle_jrk_targets(AckermannDrive):
+
+  # positive steering angle should turn left
+  target=int(AckermannDrive.steering_angle*2500+2048)
+  rospy.loginfo("target = %d",target)
+  print 'target: %s' % target
+  lowByte = (target & ord("\x1F")) | ord("\xC0")
+  highByte = (target >> 5) & ord("\x7F")
+  print("about to write", lowByte, highByte)
+  ser.write(chr(lowByte))
+  ser.write(chr(highByte))
 
 rospy.init_node('jrk_steering_node', anonymous=True)
 rospy.Subscriber(ACKERMANN_COMMAND, AckermannDrive, handle_jrk_targets)#subscribing to joy teleop
